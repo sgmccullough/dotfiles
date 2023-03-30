59fb7e542e05daf1785b9acd4242f5e87ed503eb
Scott McCullough <mcculloughsco12@gmail.com>
2023-03-30 17:05:31 +0000

    debug

--- install.sh
+++ install.sh
@@ -17,6 +17,11 @@ else
   chezmoi=chezmoi
 fi
 
+verbose=""
+if [ $RUNNER_DEBUG ]; then
+  verbose=--verbose
+fi
+
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
